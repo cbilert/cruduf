416b329bffcd5c15dc330a841b97159d07317c82
Cristian Bilert <cbilert@gmail.com>
2019-10-18 16:18:04 -0300

    teste deploy heroku

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-
-const express= require('express'),
-    path = require('path');
-
-const app = express();
-
-app.use(express.static('./dist/cruduf'));
-//app.use(express.static('./dist'));
-/*app.get('*', function (req, res) {
-  const index = path.join(__dirname, 'build', 'index.html');
-  res.sendFile(index);
-});*/
-
-app.get('/*', (req, res)=>{
-
-    //res.sendFile(path.join('','/dist/rachid-app/index.html'));
-    //res.sendFile(path.join('','/dist/rachid/index.html'));
-    //res.sendFile(path.join('','/dist/index.html'));
-    //res.sendFile(path.join('','/index.html'));
-    //res.sendFile(path.join('','/dist'));
-
-});
-
-app.listen(process.env.PORT || 8080, ()=>{
-    console.log('Server started');
-});
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/app'));
+app.listen(process.env.PORT || 3000);
